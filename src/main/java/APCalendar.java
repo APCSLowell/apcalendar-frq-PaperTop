1c2def0cbbcb441307cc17be2bfa0908884217b4
PaperTop <135560395+PaperTop@users.noreply.github.com>
2024-04-28 22:59:40 -0700

    Update APCalendar.java

--- src/main/java/APCalendar.java
+++ src/main/java/APCalendar.java
@@ -58,6 +58,6 @@ public class APCalendar
   public static int dayOfWeek(int month, int day, int year)
   {
     /* to be implemented in part (b) */
-    return ((dayOfYear(month, day, year) + firstDayOfYear(year) - 1)%7;
+    return (dayOfYear(month, day, year) + firstDayOfYear(year) - 1)%7;
   }
 }
